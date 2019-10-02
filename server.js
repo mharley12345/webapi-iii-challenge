d90865a52ad02a0a3680127d8e5e28dc10aa4a00
Mike Harley <mharley12345@gmail.com>
2019-10-02 15:23:56 -0700

    init

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = 'express';
+const express = require('express');
 
+const userRouter = require('./users/userRouter')
 const server = express();
+
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +12,15 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+ const method = req.method
+ const url = req.url
+ const time = new Date().toISOString()
+ console.log(`a ${method} request to ${url} was made @ ${time}`)
+ next()
 };
+
+
+server.use('/api/users',userRouter)
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,40 +1,105 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const User = require('./userDb')
 
-});
+
+
+
+
+router.post('/', (req, res) => {
+    User.insert(req.body)
+
+    .then(user =>{
+        res.status(201).json(user)
+    })
+    
+    .catch(error =>{
+        console.log(error)
+    res.status(500).json({message:"Error Adding User"}
+    )
+    })
+})
+
 
 router.post('/:id/posts', (req, res) => {
-
+    User.insert(req.body)
+    .then(posts =>{
+       res.status(201).json(posts)
+    })
+    .catch(error =>{
+       console.log(error,'Posting ');
+       res.status(500).json({
+           message:'Error posting '
+    })
+})
 });
 
 router.get('/', (req, res) => {
-
+     User.get(req.query)
+     .then(user =>{
+         res.status(200).json(user)
+     })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', (req, res,next) => {
+     User.getById(req.params.id)
+     .then(user =>{
+         if(user){
+             res.status(200).json(user)
+         }else{
+             res.status(404).json({message:'User not found'})
+         }
+     })
+     .catch(error =>{
+         console.log(error)
+     })
 });
 
 router.get('/:id/posts', (req, res) => {
-
+      User.getUserPosts(req.params.id)
+      .then(posts =>{
+          res.status(200).json(posts)
+      })
+      .catch(error =>{console.log(error)})
 });
 
 router.delete('/:id', (req, res) => {
-
+           User.remove(req.params.id)
+           .then(count =>{
+               if(count > 0){
+                   res.status(200).json({message:'The User Has Been Deleted'})
+               }else{
+                   res.status(404).json({message:'The User Could Not Be Found'})
+               }
+           })
+           .catch(error =>{
+               console.log(error);
+               res.status(500).json({
+                   message: 'Error Removing The User'
+               })
+           })
 });
 
 router.put('/:id', (req, res) => {
-
+    
 });
 
-//custom middleware
+// custom middleware
 
 function validateUserId(req, res, next) {
-
-};
+  const id = req.id 
+if ( id === req.param.id){
+    
+  console.log('yes')
+}else{
+    router.status(400)
+    console.log('No')
+}
+  console.log(req.params.id)
+next()
+}
 
 function validateUser(req, res, next) {
 
