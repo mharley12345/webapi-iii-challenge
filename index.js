11a5b23a4e3c790a7080684d792917e127e077c1
Mike Harley <mharley12345@gmail.com>
2019-10-03 09:14:38 -0700

    adds .env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // Imports
 //  const express = require('express');
+require('dotenv').config()
 const server = require('./server.js');
 
 
@@ -9,7 +10,7 @@ server.get('/', (req, res) => {
     res.send(`<h2>Let's write some middleware!</h2>`)
   });
 
-const port = 5000;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log('\n*** Server is running on port 5000 ***\n')
 })
